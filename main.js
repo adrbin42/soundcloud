00b5085074de28a7cf04a159df7cba5a658b2221
adrienne <adriennebing@gmail.com>
2017-06-19 23:33:15 -0400

    music player works

--- main.js
+++ main.js
@@ -12,9 +12,12 @@
 // 1. First select and store the elements you'll be working with
     // Select and store input from form elements
 
-let audioPlayer = document.querySelector(".music-player");
+let audioPlayer = document.querySelector(".music_player");
+let label = document.querySelector(".whats_playing");
 let form = document.querySelector(".search-form");
 let grid = document.querySelector(".grid");
+let searchResults = document.getElementById("search_results");
+
 
 // 2. Create your `onSubmit` event for getting the user's search term
     // Create a submit button that takes the user input and
@@ -36,7 +39,6 @@ function searchSoundCloud(song){
     let searchItems = data;
     let songs = "";
     let artist = form.search.value;
-    var uri;
 
     console.log(searchItems);
 
@@ -44,32 +46,35 @@ function searchSoundCloud(song){
       //t300x300 make artwork pics bigger?
       if(searchItems[i].artwork_url !== null){
       songs += "<div class='grid-cell'>";
-      songs += "<img src=" + searchItems[i].artwork_url + " id='artwork'><br>";
-      songs += "<span id='songTitle'>";
+      songs += "<img src=" + searchItems[i].artwork_url + " class='artwork'";
+      songs += "id='" + searchItems[i].id + "'><br>";
+      songs += "<span class='songTitle'>";
       songs += searchItems[i].title + "</span><br>";
-      songs += "<span id='artist'>";
+      songs += "<span class='artist'>";
       songs += artist.toUpperCase() + "</span></div>";
       }else{
         songs += "<div class='grid-cell'>";
-        songs += "<img src='noartwork.jpg'  id='artwork' ></span>" ;
-        songs += "<span id='songTitle'>";
-        songs += searchItems[i].title + "</span>";
-        songs += "<span id='artist'>";
+        songs += "<img src='noartwork.jpg'  class='artwork'";
+        songs += "id='" + searchItems[i].id + "'><br>" ;
+        songs += "<span class='songTitle'>";
+        songs += searchItems[i].title + "</span><br>";
+        songs += "<span class='artist'>";
         songs += artist.toUpperCase() + "</span></div>";
         }
       }
-      grid.innerHTML = "<div class='searchText'><h3>Search results</h3></div>" + songs;
+      grid.innerHTML = songs;
     });
   });
 
 }
 // 5. Create a way to listen for a click that will play the song in the audio play
     // Add an event listener that listens for a click event.
-
-    // Get the parent DIV, add click listener...
-  document.querySelector(".grid").addEventListener("click", function(event) {
-	// event.target was the clicked element
-  if (event.target && event.target.matches(".grid-cell")) {
-    console.log("Search result clicked!");
-	}
+searchResults.addEventListener('click',function(e){
+  console.log(e.target.id);
+  if(e.target && e.target.matches("img.artwork")){
+  let str_uri = "https://api.soundcloud.com/tracks/" + e.target.id + "/stream?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f";
+  audioPlayer.src = str_uri;
+  audioPlayer.autoplay = true;
+  label.innerHTML = "<p><strong>Now playing: " + e.target.alt + "</strong></p>";
+  }
 });
